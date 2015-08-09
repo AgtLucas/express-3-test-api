4ada5feaa4d5d4cfad770ac07cd38d8c70a6a43c
Lucas <lucas@agtlucas.com>
2015-08-09 09:06:03 -0300

    Update app.js

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ var path = require('path');
 var join = path.join;
 var express = require('express');
 var app = express();
+var request = require('superagent');
 
 var api = 'https://api.forecast.io/forecast/';
 var token = process.env.FORECAST_TOKEN;
@@ -13,8 +14,16 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 app.use(express.static(__dirname + '/public'));
 
-app.get('/', function(req, res) {
-  res.render('index', {title: 'E3'});
+app.get('/le-tempo/forecast/:ll', function(req, res) {
+  request
+    .get(api + token + '/' + req.params.ll)
+    .set('Content-Type', 'application/json')
+    .accept('application/json')
+    .end(function(e, data) {
+      if (e) next(e);
+      console.log(data.body);
+      return res.render('index', {data: data.body});
+    });
 });
 
 app.listen(3000);
