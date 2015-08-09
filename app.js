da70f28f581e3aea2f7af9b4093c04e29bcd9df5
Lucas <lucas@agtlucas.com>
2015-08-09 08:47:52 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1,5 +1,19 @@
 'use strict';
 
+var path = require('path');
+var join = path.join;
 var express = require('express');
 var app = express();
 
+var api = 'https://api.forecast.io/forecast/';
+var token = process.env.FORECAST_TOKEN;
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+app.use(express.logger());
+app.use(express.static(__dirname + '/public'));
+
+app.get('/', function(req, res) {
+  res.render('index', {title: 'E3'});
+});
